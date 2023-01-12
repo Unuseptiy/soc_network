53f14f5b0e8c7d83597cf367d02dee299b81543c
Kozenko Valeriy <Osmiylantan@mail.ru>
2023-01-12 17:48:30 +0300

    Add project __init__.

---
+++ soc_network/__init__.py
@@ -0,0 +1,18 @@
+"""
+Сервис с REST API, для работы с постами в соц сети.
+"""
+__author__ = "Kozenko Valeriy"
+__maintainer__ = __author__
+
+__email__ = "osmiylantan@mail.ru"
+__license__ = ""
+__version__ = "0.1.0"
+
+
+__all__ = (
+    "__author__",
+    "__email__",
+    "__license__",
+    "__maintainer__",
+    "__version__",
+)
