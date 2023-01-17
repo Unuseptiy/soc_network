ed1f9a84cd28f3528e45edb2d272e9615b3acee9
Kozenko Valeriy <Osmiylantan@mail.ru>
2023-01-17 23:02:20 +0300

    Fix ping db bug, when db is unavailable.

--- soc_network/services/health_check/database.py
+++ soc_network/services/health_check/database.py
@@ -3,5 +3,8 @@ from sqlalchemy import select, text
 
 async def health_check_db(session) -> bool:
     health_check_query = select(text("1"))
-    result = await session.scalars(health_check_query)
-    return result is not None
+    try:
+        result = await session.scalars(health_check_query)
+        return result is not None
+    except OSError:
+        return False
